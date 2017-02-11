52fb69fa6c8bce8bcb4a6096053f0a9ff040c940
Julien Tilap LA VINH <jlavinh@gmail.com>
2017-02-11 12:42:37 +0100

    plop

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 import UrlGenerator from './Generator';
 
-export default function (...args) {
+export default function (args) {
   const generator = new UrlGenerator();
-  args.forEach(arg => generator.add(arg));
+  args.forEach(arg => generator.add(...arg));
   return generator.toString();
 }
